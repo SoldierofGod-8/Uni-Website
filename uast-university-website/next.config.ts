04a84137cc4b8def7f167195e436a95a051b91cc
SoldierofGod-8 <wamisetech@gmail.com>
2026-06-13 13:18:35 +0100

    BasePath Edit

--- uast-university-website/next.config.ts
+++ uast-university-website/next.config.ts
@@ -1,7 +1,7 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: '/uast-university-website', // Required: Tells Next.js your site is in a subfolder
+  basePath: '/Uni-Website', // Required: Tells Next.js your site is in a subfolder
 };
 
 export default nextConfig;
