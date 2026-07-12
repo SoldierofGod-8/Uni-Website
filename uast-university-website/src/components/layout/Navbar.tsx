5afd02a59ededc29a011a13ce975ee5b30bfa091
SoldierofGod-8 <wamisetech@gmail.com>
2026-07-13 00:57:20 +0100

    Move Job Opportunities to Campus dropdown in navbar

--- uast-university-website/src/components/layout/Navbar.tsx
+++ uast-university-website/src/components/layout/Navbar.tsx
@@ -30,6 +30,7 @@ const MENU_GROUPS = [
       { href: "/staff", label: "Staff" },
       { href: "/partnership", label: "Partnerships" },
       { href: "/endowment", label: "Endowment" },
+      { href: "/careers", label: "Job Opportunities" },
       { href: "/athletics", label: "Athletics" },
     ],
   },
